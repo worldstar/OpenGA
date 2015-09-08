4435bb42575dd4af7a53cc970bab1b55d7558f60
user <user@appledeMacBook-Air.local>
2015-09-08 16:24:58 +0800

    Fix an import problem
    Add a missing import expression.

--- src/openga/ObjectiveFunctions/TPObjectiveFunctionMTSP.java
+++ src/openga/ObjectiveFunctions/TPObjectiveFunctionMTSP.java
@@ -1,5 +1,5 @@
 package openga.ObjectiveFunctions;
-
+import openga.chromosomes.*;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
