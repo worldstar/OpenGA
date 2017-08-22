9c09e0613b969e22f1d6c9f2929b3857985615ff
Kuo Yu-Cheng <s74101234@gmail.com>
2017-08-22 16:06:44 +0800

    upload ObjFunctionPFSSOAWT
    upload ObjFunctionPFSSOAWT.java
    upload ObjFunctionPFSSOAWTI.java
    building interface

---
+++ src/openga/ObjectiveFunctions/ObjFunctionPFSSOAWTI.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package openga.ObjectiveFunctions;
+
+import openga.applications.flowshopProblem.*;
+import openga.ObjectiveFunctions.ObjectiveFunctionFlowShopScheduleI;
+
+/**
+ *
+ * @author Kuo Yu-Cheng
+ */
+public interface ObjFunctionPFSSOAWTI extends ObjectiveFunctionFlowShopScheduleI {
+  void setOASData(int[] pi , int[] di , Double[] wi , Double[] pal , Double[] profit);
+}
