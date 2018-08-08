9dbe350ce4bbb017e20a461694294ba827d25f96
Kuo,Yu-Cheng <s74101234@gmail.com>
2018-08-08 10:57:38 +0800

    upload PBILInteractiveWithEDA3_2_2.java
    upload PBILInteractiveWithEDA3_2_2.java
    revise PBILInteractiveWithEDA3_2.java
    revise singleThreadGAwithEDA3_2.java

--- src/openga/MainProgram/singleThreadGAwithEDA3_2.java
+++ src/openga/MainProgram/singleThreadGAwithEDA3_2.java
@@ -26,7 +26,8 @@ public class singleThreadGAwithEDA3_2 extends singleThreadGAwithEDA2 implements 
     int D2 = 0;
     
 //    PBILInteractiveWithEDA3 PBIL1;   //PBIL
-    PBILInteractiveWithEDA3_2 PBIL1;   //PBIL
+//    PBILInteractiveWithEDA3_2 PBIL1;   //PBIL
+    PBILInteractiveWithEDA3_2_2 PBIL1;
 
     public void startGA() {
         Population = initialStage();
@@ -38,7 +39,8 @@ public class singleThreadGAwithEDA3_2 extends singleThreadGAwithEDA2 implements 
         archieve = findParetoFront(Population, 0);
 
 //        PBIL1 = new PBILInteractiveWithEDA3(Population, lamda, beta);   // PBIL
-        PBIL1 = new PBILInteractiveWithEDA3_2(Population, lamda, beta , D1 , D2);   // PBIL
+//        PBIL1 = new PBILInteractiveWithEDA3_2(Population, lamda, beta , D1 , D2);   // PBIL
+        PBIL1 = new PBILInteractiveWithEDA3_2_2(Population, lamda, beta , D1 , D2);   // PBIL
 
         container = PBIL1.getContainer();
         inter = PBIL1.getInter();
