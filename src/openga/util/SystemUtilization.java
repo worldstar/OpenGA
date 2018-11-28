d2baae98c52ec47c371f8ac4dff639eea51665ac
Kuo,Yu-Cheng <s74101234@gmail.com>
2018-11-28 16:13:10 +0800

    update SystemUtilization.java
    update SystemUtilization.java

--- src/openga/util/SystemUtilization.java
+++ src/openga/util/SystemUtilization.java
@@ -8,58 +8,112 @@ package openga.util;
 import java.io.IOException;
 import com.sun.management.OperatingSystemMXBean;
 import java.lang.management.ManagementFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author user2
  */
 public class SystemUtilization {     
-  public static void checkSystemStatus(double utilizationRate) throws InterruptedException, IOException{
+  public static void checkSystemStatus(double utilizationRate){
     int busyCounter = 0;
     int freeCounter = 0;
     OperatingSystemMXBean osBean = (com.sun.management.OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean();
         
-    while (true) {
-        Thread.sleep(1000);
-        
-        double loading = osBean.getSystemCpuLoad();//getProcessCpuLoad()
-        System.out.printf("Loading: %.4f \n", loading);
-        
-        if(loading == 1 || loading < 0 || loading == Double.NaN){
-          Thread.sleep(2000);
-          continue;
+    try {
+        while (true) {
+            Thread.sleep(1000);
+
+            double loading = osBean.getSystemCpuLoad();//getProcessCpuLoad()
+            System.out.printf("Loading: %.4f \n", loading);
+
+            if(loading == 1 || loading < 0 || loading == Double.NaN){
+              Thread.sleep(2000);
+              continue;
+            }
+            else if(loading > utilizationRate) { 
+                busyCounter ++;
+
+              if(busyCounter > 3){
+    //            System.out.println("Busying");
+                break;
+              }            
+            }
+            else{
+              freeCounter ++;
+
+              if(freeCounter > 3){
+                shutdownDirectly();
+    //            System.out.println("Shutdown");
+                break;
+              }            
+            }                            
         }
-        else if(loading > utilizationRate) { 
-            busyCounter ++;
-            
-          if(busyCounter > 3){
-//            System.out.println("Busying");
-            break;
-          }            
-        }
-        else{
-          freeCounter ++;
-          
-          if(freeCounter > 3){
-            shutdownDirectly();
-//            System.out.println("Shutdown");
-            break;
-          }            
-        }                            
-    }    
+    } catch (InterruptedException ex) {
+      Logger.getLogger(SystemUtilization.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    
   }
   
-  public static void checkSystemStatus() throws InterruptedException, IOException{
+  public static void checkSystemStatus(){
     checkSystemStatus(0.01);// 0.01 = 1% CPU Utilization
   }  
   
-  public static void shutdownDirectly() throws IOException, InterruptedException{
+  public static void shutdownDirectly(){
     String command = "sudo shutdown -h now";//Linux command
     
     if(System.getProperty("os.name").startsWith("Windows")){
       command = "shutdown -s -t 0";
     }
     
-    Process p = Runtime.getRuntime().exec(command);
-    p.waitFor();    
+    try {
+      Process p = Runtime.getRuntime().exec(command);
+      p.waitFor();    
+    } catch (IOException ex) {
+      Logger.getLogger(SystemUtilization.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (InterruptedException ex) {
+      Logger.getLogger(SystemUtilization.class.getName()).log(Level.SEVERE, null, ex);
+    }
   }    
+  
+    public void incrementCounter(String fileName, int maxCounter){
+
+      int Counter = readCount(fileName);
+      if(Counter == maxCounter){
+          shutdownDirectly();
+      }else{
+          writeCount(fileName,Counter);
+      }
+  }
+
+  public int readCount(String fileName){
+      String Count = "";
+      try {
+          FileReader fr = new FileReader(fileName);
+          BufferedReader br = new BufferedReader(fr);
+          String eachLine = "";
+          while ((eachLine = br.readLine()) != null) {
+            Count += eachLine;
+          }
+          fr.close();
+      } catch (IOException ex) {
+          Count = "1";
+      }
+      return Integer.parseInt(Count);
+  }
+
+  public void writeCount(String fileName,int Counter){
+      try {
+          FileWriter fw = new FileWriter(fileName);
+          Counter += 1;
+          fw.write(String.valueOf(Counter));
+          fw.close();
+      } catch (IOException ex) {
+          Logger.getLogger(SystemUtilization.class.getName()).log(Level.SEVERE, null, ex);
+      }
+  }
 }
